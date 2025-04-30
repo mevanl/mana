ffdfeb40d47040b7fee03d27c006395576e63125
mevanl <mevanlauderdaleyt@gmail.com>
2025-04-30 09:54:51 -0500

    Fleshed out recover middleware to be more useful in debugging

--- backend/internal/middleware/recover.go
+++ backend/internal/middleware/recover.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"log"
 	"net/http"
+	"runtime/debug"
 )
 
 func Recover(next http.Handler) http.Handler {
@@ -12,7 +13,8 @@ func Recover(next http.Handler) http.Handler {
 		defer func() {
 			if recover := recover(); recover != nil {
 				// log panic error
-				log.Printf("Panic recovered: %+v", recover)
+				log.Printf("Panic recovered: %+v\nRequest: %s %s\nStack trace:\n%s",
+					recover, r.Method, r.URL.Path, string(debug.Stack()))
 
 				// return 500 (internal server error)
 				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
