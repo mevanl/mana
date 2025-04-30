8d928bfbc07215b8fecad845165c409397a1b3db
mevanl <mevanlauderdaleyt@gmail.com>
2025-04-30 13:40:50 -0500

    Loaded env

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -5,9 +5,17 @@ import (
 	"mana/internal/api"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("ERROR: Failed loading .env file.")
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -15,8 +23,9 @@ func main() {
 
 	router := api.NewRouter()
 
+	// Start server
 	log.Printf("Mana server on port %s...\n", port)
-	err := http.ListenAndServe(":"+port, router)
+	err = http.ListenAndServe(":"+port, router)
 	if err != nil {
 		log.Fatalf("Server failed: %s", err)
 	}
