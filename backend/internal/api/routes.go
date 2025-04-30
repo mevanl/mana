030591dcef3a14a4b1f975c18bc48b23bdec66e8
mevanl <mevanlauderdaleyt@gmail.com>
2025-04-30 12:56:24 -0500

    Setup router to use middleware

--- backend/internal/api/routes.go
+++ backend/internal/api/routes.go
@@ -3,6 +3,7 @@ package api
 import (
 	"mana/internal/middleware"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -13,6 +14,10 @@ func NewRouter() http.Handler {
 	// Middleware
 	router.Use(middleware.Recover)
 	router.Use(middleware.Logging)
+	router.Use(middleware.CORS)
+	router.Use(middleware.SecurityHeaders)
+	router.Use(middleware.Timeout(10 * time.Second))
+	router.Use(middleware.Authenticate)
 
 	// get health endpoint
 	router.Get("/api/v1/health", Health_Handler)
