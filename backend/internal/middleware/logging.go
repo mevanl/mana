20b2c27035f10487709b916badf2c366f393db68
mevanl <mevanlauderdaleyt@gmail.com>
2025-04-30 09:50:42 -0500

    Updated Logging middleware to include statuscode

--- backend/internal/middleware/logging.go
+++ backend/internal/middleware/logging.go
@@ -6,13 +6,33 @@ import (
 	"time"
 )
 
-// logs a request with: [timestamp_start] [http method] [request_url] [duration]
+// logs the HTTP method, path, response status, and duration for each request.
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start_time := time.Now()
-		next.ServeHTTP(w, r)
+
+		lrw := &loggingResponseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+
+		next.ServeHTTP(lrw, r)
 		duration_time := time.Since(start_time)
 
-		log.Printf("[%s] %s %s %s", start_time.Format(time.RFC3339), r.Method, r.URL.Path, duration_time)
+		log.Printf("[%s] %s %s %d %s",
+			start_time.Format(time.RFC3339),
+			r.Method,
+			r.URL.Path,
+			lrw.statusCode,
+			duration_time,
+		)
 	})
+}
+
+// Catches status code
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *loggingResponseWriter) WriterHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
 }
